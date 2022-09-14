61396af7d790f04e10b0fc6ef274794c3cb82939
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-14 17:48:53 -0300

    chore: Req 04 (Middlewares) - Cria middle de erro para tratar erros gerados pelo ZOD

---
+++ src/middlewares/error.ts
@@ -0,0 +1,20 @@
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+
+const errorHandler:ErrorRequestHandler = (
+  err: Error | ZodError,
+  _req,
+  res,
+  _next,
+) => {
+  if (err instanceof ZodError) {
+    return res.status(400).json({ message: err.issues });
+  }
+
+  // const messageAsErrorType = err.message as keyof typeof ErrorTypes;
+
+  console.log(err);
+  return res.status(500).json({ message: 'internal error' });
+};
+
+export default errorHandler;
